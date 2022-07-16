7e64e52c149d8a11ac2cafcc1a19aef931ec1cd9
Ander <32654298+oanderoficial@users.noreply.github.com>
2022-07-16 16:42:01 -0300

    Update wiresec.sh

--- wiresec.sh
+++ wiresec.sh
@@ -20,7 +20,7 @@ echo -e $red" ___      _____(_)_____________ _____________ _______ "
 echo -e $red" __ | /| / /__  / __  ___/_  _ \__  ___/_  _ \_  ___/ "
 echo -e $red" __ |/ |/ / _  /  _  /    /  __/_(__  ) /  __// /__   "
 echo -e $red" ____/|__/  /_/   /_/     \___/ /____/  \___/ \___/   "
-echo -e $yellow" Autor:  Anderson.b.Silva | 35 Tools | GIT |$green v.1.1 "
+echo -e $yellow" Autor:  Anderson.b.Silva | 37 Tools | GIT |$green v.1.1 "
 echo ""
 echo -e $yellow "[#]"$red "*** Wireless Attacks *** "
 echo -e $red " *** É necessário ter o git instalado ***"
@@ -30,7 +30,7 @@ echo -e $okegreen "[!] Os Arquivos baixados se encontram em /wiresec"
 echo -e $orange
 echo ""
 
-select i in  Aircrack/ng Asleap Airflood Airgeddon Airopy Airoscript Bluelog BlueMaho Bluepot BlueRanger Bluesnarfer Bully coWPAtty crackle eapmd5pass Fern/Wifi/Cracker fluxion Ghost/Phisher GISKismet gr-scan kalibrate-rtl KillerBee Kismet mdk3 mfcuk mfoc mfterm Multimon-NG PixieWPS Reaver redfang RTLSDR-Scanner Spooftooph Wifi-Honey Wifitap Wifite Todos sair
+select i in  Aircrack/ng Asleap Airflood Airgeddon Airopy Airoscript airpwn Bluelog BlueMaho Bluepot BlueRanger Bluesnarfer Bully coWPAtty crackle eapmd5pass Fern/Wifi/Cracker fluxion Ghost/Phisher GISKismet gr-scan kalibrate-rtl KillerBee Kismet mdk3 mfcuk mfoc mfterm Multimon-NG PixieWPS Reaver redfang RTLSDR-Scanner Spooftooph Wifi-Honey Wifitap Wifite Todos sair
 do
 
    case "$i" in
@@ -130,6 +130,23 @@ echo -e ""
    echo -e ""
 
          echo -e "$okegreen [TOOL: Airoscript]"
+         echo -e ""
+      echo -e " $okegreen [!] Download Completo! Os Arquivos baixados se encontram em /wiresec"
+      echo -e "$cyan  [!] Para chamar o menu de opções pressione ENTER "
+      echo -e "$orange wiresec v.1.1"
+    ;;
+
+   airpwn)
+   echo -e "$yellow *** Baixando airpwn ***"
+   git clone https://github.com/ICSec/airpwn-ng.git
+   clear
+   echo -e "   __      __ (_)  _ __    ___   ___    ___    ___ "
+   echo -e "  \ \ /\ / / | | | '__|  / _ \ / __|  / _ \  / __| "
+   echo -e "   \ V  V /  | | | |    |  __/ \__ \ |  __/ | (__  "
+   echo -e "    \_/\_/   |_| |_|     \___| |___/  \___|  \___| "
+   echo -e ""
+
+         echo -e "$okegreen [TOOL: airpwn]"
          echo -e ""
       echo -e " $okegreen [!] Download Completo! Os Arquivos baixados se encontram em /wiresec"
       echo -e "$cyan  [!] Para chamar o menu de opções pressione ENTER "
@@ -665,6 +682,9 @@ echo -e ""
     echo -e "$okegreen [!] Download Completo!"
     echo -e "$yellow [********************************]"
     git clone https://github.com/patrickhno/Airoscript.git
+    echo -e "$okegreen [!] Download Completo!"
+    echo -e "$yellow [********************************]"
+    git clone https://github.com/ICSec/airpwn-ng.git
     echo -e "$okegreen [!] Download Completo!"
     echo -e "$yellow [********************************]"
     git clone https://github.com/MS3FGX/Bluelog.git
