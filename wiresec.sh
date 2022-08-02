93470c1d9c06534ea8ba9ef6a75bacc62d734b75
Ander <32654298+oanderoficial@users.noreply.github.com>
2022-08-02 20:24:41 -0300

    Update wiresec.sh

--- wiresec.sh
+++ wiresec.sh
@@ -27,7 +27,7 @@ echo -e $okegreen "[!] Os Arquivos baixados se encontram em /wiresec"
 echo -e $orange
 echo ""
 
-select i in  Aircrack/ng Asleap Airflood Airgeddon Airopy Airoscript airpwn aphopper apnbf atear auto-eap batctl batman-adv Bluelog BlueMaho Bluepot BlueRanger Bluesnarfer Bully coWPAtty crackle eapmd5pass Fern/Wifi/Cracker fluxion Ghost/Phisher GISKismet gr-scan kalibrate-rtl KillerBee Kismet mdk3 mfcuk mfoc mfterm Multimon-NG PixieWPS Reaver redfang RTLSDR-Scanner Spooftooph Wifi-Honey Wifitap Wifite Todos sair
+select i in  Aircrack/ng Asleap Airflood Airgeddon Airopy Airoscript airpwn aphopper apnbf atear auto-eap batctl batman-adv batman-alfred	Bluelog BlueMaho Bluepot BlueRanger Bluesnarfer Bully coWPAtty crackle eapmd5pass Fern/Wifi/Cracker fluxion Ghost/Phisher GISKismet gr-scan kalibrate-rtl KillerBee Kismet mdk3 mfcuk mfoc mfterm Multimon-NG PixieWPS Reaver redfang RTLSDR-Scanner Spooftooph Wifi-Honey Wifitap Wifite Todos sair
 do
 
    case "$i" in
@@ -247,6 +247,23 @@ echo -e "    \_/\_/   |_| |_|     \___| |___/  \___|  \___| "
 echo -e ""
 
 echo -e "$okegreen [TOOL: batman-adv]"
+echo -e ""
+echo -e "$okegreen [!] Download Completo! Os Arquivos baixados se encontram em /wiresec"
+echo -e "$cyan  [!] Para chamar o menu de opções pressione ENTER "
+echo -e "$orange wiresec v.1.2"
+;;
+
+batman-alfred)
+echo -e "$yellow *** Baixando batman-alfred***"
+git clone https://github.com/nomaster/aur-batman-alfred.git
+clear
+echo -e "   __      __ (_)  _ __    ___   ___    ___    ___ "
+echo -e "  \ \ /\ / / | | | '__|  / _ \ / __|  / _ \  / __| "
+echo -e "   \ V  V /  | | | |    |  __/ \__ \ |  __/ | (__  "
+echo -e "    \_/\_/   |_| |_|     \___| |___/  \___|  \___| "
+echo -e ""
+
+echo -e "$okegreen [TOOL: batman-alfred]"
 echo -e ""
 echo -e "$okegreen [!] Download Completo! Os Arquivos baixados se encontram em /wiresec"
 echo -e "$cyan  [!] Para chamar o menu de opções pressione ENTER "
@@ -802,6 +819,9 @@ https://github.com/open-mesh-mirror/batctl.git
 echo -e "$okegreen [!] Download Completo!"
 echo -e "$yellow [********************************]"
 git clone https://github.com/open-mesh-mirror/batman-adv.git
+echo -e "$okegreen [!] Download Completo!"
+echo -e "$yellow [********************************]"
+https://github.com/nomaster/aur-batman-alfred.git
 echo -e "$okegreen [!] Download Completo!"
 echo -e "$yellow [********************************]"
 git clone https://github.com/MS3FGX/Bluelog.git
