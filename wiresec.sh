df97d376502760848e3588be09ebd7e77a90eb8e
Ander <32654298+oanderoficial@users.noreply.github.com>
2022-08-09 18:32:46 -0300

    New tool (beholder)

--- wiresec.sh
+++ wiresec.sh
@@ -17,7 +17,7 @@ echo -e $red" ___      _____(_)_____________ _____________ _______ "
 echo -e $red" __ | /| / /__  / __  ___/_  _ \__  ___/_  _ \_  ___/ "
 echo -e $red" __ |/ |/ / _  /  _  /    /  __/_(__  ) /  __// /__   "
 echo -e $red" ____/|__/  /_/   /_/     \___/ /____/  \___/ \___/   "
-echo -e $yellow" Autor:  Anderson.b.Silva | 44 Tools | GIT |$green v.1.2 "
+echo -e $yellow" Autor:  Anderson.b.Silva | 45 Tools | GIT |$green v.1.2 "
 echo ""
 echo -e $yellow "[#]"$red "*** Wireless Attacks *** "
 echo -e $red " *** É necessário ter o git instalado ***"
@@ -27,7 +27,7 @@ echo -e $okegreen "[!] Os Arquivos baixados se encontram em /wiresec"
 echo -e $orange
 echo ""
 
-select i in  Aircrack/ng Asleap Airflood Airgeddon Airopy Airoscript airpwn aphopper apnbf atear auto-eap batctl batman-adv batman-alfred boopsuite Bluelog BlueMaho Bluepot BlueRanger Bluesnarfer Bully coWPAtty crackle eapmd5pass Fern/Wifi/Cracker fluxion Ghost/Phisher GISKismet gr-scan kalibrate-rtl KillerBee Kismet mdk3 mfcuk mfoc mfterm Multimon-NG PixieWPS Reaver redfang RTLSDR-Scanner Spooftooph Wifi-Honey Wifitap Wifite Todos sair
+select i in  Aircrack/ng Asleap Airflood Airgeddon Airopy Airoscript airpwn aphopper apnbf atear auto-eap batctl batman-adv batman-alfred beholder boopsuite Bluelog BlueMaho Bluepot BlueRanger Bluesnarfer Bully coWPAtty crackle eapmd5pass Fern/Wifi/Cracker fluxion Ghost/Phisher GISKismet gr-scan kalibrate-rtl KillerBee Kismet mdk3 mfcuk mfoc mfterm Multimon-NG PixieWPS Reaver redfang RTLSDR-Scanner Spooftooph Wifi-Honey Wifitap Wifite Todos sair
 do
 
    case "$i" in
@@ -270,6 +270,22 @@ echo -e "$cyan  [!] Para chamar o menu de opções pressione ENTER "
 echo -e "$orange wiresec v.1.2"
 ;;
 
+beholder)
+echo -e "$yellow *** Baixando beholder	***"
+git clone https://github.com/nmurilo/BeholderWifi.git
+clear
+echo -e "   __      __ (_)  _ __    ___   ___    ___    ___ "
+echo -e "  \ \ /\ / / | | | '__|  / _ \ / __|  / _ \  / __| "
+echo -e "   \ V  V /  | | | |    |  __/ \__ \ |  __/ | (__  "
+echo -e "    \_/\_/   |_| |_|     \___| |___/  \___|  \___| "
+echo -e ""
+
+echo -e "$okegreen [TOOL:beholder]"
+echo -e ""
+echo -e "$okegreen [!] Download Completo! Os Arquivos baixados se encontram em /wiresec"
+echo -e "$cyan  [!] Para chamar o menu de opções pressione ENTER "
+echo -e "$orange wiresec v.1.2"
+;;
 
 boopsuite)
 echo -e "$yellow *** Baixando boopsuite***"
@@ -840,6 +856,9 @@ git clone https://github.com/open-mesh-mirror/batman-adv.git
 echo -e "$okegreen [!] Download Completo!"
 echo -e "$yellow [********************************]"
 https://github.com/nomaster/aur-batman-alfred.git
+echo -e "$okegreen [!] Download Completo!"
+echo -e "$yellow [********************************]"
+git clone https://github.com/nmurilo/BeholderWifi.git
 echo -e "$okegreen [!] Download Completo!"
 echo -e "$yellow [********************************]"
 git clone https://github.com/MisterBianco/BoopSuite.git
