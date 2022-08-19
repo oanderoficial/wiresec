cd9f43a1a813d5e0e0936280b86092af82780095
Ander <32654298+oanderoficial@users.noreply.github.com>
2022-08-19 17:44:37 -0300

    update tool create_ap

--- wiresec.sh
+++ wiresec.sh
@@ -17,7 +17,7 @@ echo -e $red" ___      _____(_)_____________ _____________ _______ "
 echo -e $red" __ | /| / /__  / __  ___/_  _ \__  ___/_  _ \_  ___/ "
 echo -e $red" __ |/ |/ / _  /  _  /    /  __/_(__  ) /  __// /__   "
 echo -e $red" ____/|__/  /_/   /_/     \___/ /____/  \___/ \___/   "
-echo -e $yellow" Autor:  Anderson.b.Silva | 45 Tools | GIT |$green v.1.2 "
+echo -e $yellow" Autor:  Anderson.b.Silva | 46 Tools | GIT |$green v.1.2 "
 echo ""
 echo -e $yellow "[#]"$red "*** Wireless Attacks *** "
 echo -e $red " *** É necessário ter o git instalado ***"
@@ -27,7 +27,7 @@ echo -e $okegreen "[!] Os Arquivos baixados se encontram em /wiresec"
 echo -e $orange
 echo ""
 
-select i in  Aircrack/ng Asleap Airflood Airgeddon Airopy Airoscript airpwn aphopper apnbf atear auto-eap batctl batman-adv batman-alfred beholder boopsuite Bluelog BlueMaho Bluepot BlueRanger Bluesnarfer Bully coWPAtty crackle eapmd5pass Fern/Wifi/Cracker fluxion Ghost/Phisher GISKismet gr-scan kalibrate-rtl KillerBee Kismet mdk3 mfcuk mfoc mfterm Multimon-NG PixieWPS Reaver redfang RTLSDR-Scanner Spooftooph Wifi-Honey Wifitap Wifite Todos sair
+select i in  Aircrack/ng Asleap Airflood Airgeddon Airopy Airoscript airpwn aphopper apnbf atear auto-eap batctl batman-adv batman-alfred beholder boopsuite Bluelog BlueMaho Bluepot BlueRanger Bluesnarfer Bully coWPAtty crackle create_ap eapmd5pass Fern/Wifi/Cracker fluxion Ghost/Phisher GISKismet gr-scan kalibrate-rtl KillerBee Kismet mdk3 mfcuk mfoc mfterm Multimon-NG PixieWPS Reaver redfang RTLSDR-Scanner Spooftooph Wifi-Honey Wifitap Wifite Todos sair
 do
 
    case "$i" in
@@ -434,6 +434,23 @@ echo -e "    \_/\_/   |_| |_|     \___| |___/  \___|  \___| "
 echo -e ""
 
 echo -e "$okegreen [TOOL: crackle]"
+echo -e ""
+echo -e "$okegreen [!] Download Completo! Os Arquivos baixados se encontram em /wiresec"
+echo -e "$cyan  [!] Para chamar o menu de opções pressione ENTER "
+echo -e "$orange wiresec v.1.2"
+;;
+
+create_ap)
+echo -e "$yellow *** Baixando create_ap***"
+git clone https://github.com/oblique/create_ap.git
+clear
+echo -e "   __      __ (_)  _ __    ___   ___    ___    ___ "
+echo -e "  \ \ /\ / / | | | '__|  / _ \ / __|  / _ \  / __| "
+echo -e "   \ V  V /  | | | |    |  __/ \__ \ |  __/ | (__  "
+echo -e "    \_/\_/   |_| |_|     \___| |___/  \___|  \___| "
+echo -e ""
+
+echo -e "$okegreen [TOOL: cracreate_apkle]"
 echo -e ""
 echo -e "$okegreen [!] Download Completo! Os Arquivos baixados se encontram em /wiresec"
 echo -e "$cyan  [!] Para chamar o menu de opções pressione ENTER "
@@ -883,6 +900,9 @@ git clone https://github.com/roobixx/cowpatty.git
 echo -e "$okegreen [!] Download Completo!"
 echo -e "$yellow [********************************]"
 git clone https://github.com/mikeryan/crackle.git
+echo -e "$okegreen [!] Download Completo!"
+echo -e "$yellow [********************************]"
+git clone https://github.com/oblique/create_ap.git
 echo -e "$okegreen [!] Download Completo!"
 echo -e "$yellow [********************************]"
 git clone https://github.com/joswr1ght/eapmd5pass.git
